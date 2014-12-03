7377e960d04e25aab5ef27458d160502515094d1
omeid <public@omeid.me>
2014-12-03 09:48:56 -0500

    Pin progress. Refactoring... 2

--- engine/engine.go
+++ engine/engine.go
@@ -1,6 +1,8 @@
 package engine
 
 import (
+	"sync"
+
 	"github.com/forklift/geppetto/event"
 	"github.com/forklift/geppetto/unit"
 )
@@ -8,130 +10,59 @@ import (
 func New() (*Engine, error) {
 
 	e := &Engine{
-	//	make(chan unit.Event),
+		Transactions: NewTransactionList(),
+		Events:       make(chan *event.Event),
 	}
 
 	return e, nil
 }
 
 type Engine struct {
-	//A units registery, holds all the units.
-	units map[string]*Transaction
 
 	//All le transactions.
-	Transactions map[string]*Transaction
-	Events       chan<- *event.Event
+	Transactions TransactionList
+
+	Events chan *event.Event
 
 	//Internals.
-	//One transaction at a time?
-	//       lock sync.Mutex
+	//One transaction at a time
+	lock sync.Mutex
 }
 
-func (e *Engine) Start(t *unit.Unit) error {
+func (e *Engine) Start(u *unit.Unit) error {
 
-	//One Transaction at a time?
-	//e.lock.Lock()
-	//defer e.lock.Unlock()
+	//One Transaction at a time
+	e.lock.Lock()
+	defer e.lock.Unlock()
 	var transaction *Transaction
 
-	if u, ok := e.units[t.Name]; ok {
-		e.Events <- event.NewEvent(u.unit.Name, event.StatusAlreadyLoaded)
+	if t, ok := e.Transactions.Get(u.Name); ok {
+		e.Events <- event.NewEvent(t.unit.Name, event.StatusAlreadyLoaded)
 
-		e.Events <- event.NewEvent(u.unit.Name, event.StatusTransactionRegistering)
+		e.Events <- event.NewEvent(t.unit.Name, event.StatusTransactionRegistering)
 		//TODO: Health check? Status Check?
-		e.Transactions[u.unit.Name] = u
-		e.Events <- event.NewEvent(u.unit.Name, event.StatusTransactionRegistered)
+		t.Explicit = true
+		e.Events <- event.NewEvent(t.unit.Name, event.StatusTransactionRegistered)
 		return nil
 
 	} else {
-		transaction = NewTransaction(e, t)
+		transaction = NewTransaction(e, u)
+		t.Explicit = true
 	}
 
-	//TODO: prepare, health, add.
-	return transaction.Start()
-
-}
-
-func (e *Engine) requestUnits(units []string) (map[string]*Transaction, error) {
-
-	all := make(map[string]*Transaction)
-
-	errs := make(chan error)
-	cancel := make(chan struct{})
-	transactions := prepareTransactions(e, errs, cancel, units)
-
-	end := make(chan struct{})
-
-	go func() {
-		defer close(end)
-		for t := range transactions {
-			all[t.unit.Name] = t
-		}
-	}()
-
-	select {
-	case err := <-errs:
-		if err != nil {
-			close(cancel)
-
-			//TODO: Clean up in the background. TODO: Pass the events? how do you log the progress?
-			//go func() {
-			<-end //Wait for all units.
-			for _, t := range all {
-				t.unit.Service.Cleanup() //TODO: Log/Handle errors
-			}
-			//	}()
-			return nil, err //TODO: should retrun the units so fa?
-		}
-	case <-end:
-		return all, nil
-	}
-	//TODO: Timeout?
-
-	//TODO: We shouldn't really ever reach here. Panic? Error?
-	return all, nil
-}
-
-func (e *Engine) requestUnit(name string) (*Transaction, error) {
-	if u, ok := e.units[name]; ok {
-		return u, nil
-	}
-	u, err := unit.Read(name)
+	err := transaction.Prepare()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return NewTransaction(e, u), nil
-}
+	err = transaction.Start()
+	if err != nil {
+		return err
+	}
 
-func prepareTransactions(engine *Engine, errs chan<- error, cancel <-chan struct{}, names []string) <-chan *Transaction {
+	//TODO: Health check.
 
-	transactions := make(chan *Transaction)
-	go func() {
-		defer close(transactions)
-		for _, name := range names {
+	e.Transactions.Append(&transaction.deps)
 
-			transaction, err := engine.requestUnit(name)
-
-			if err == nil {
-				err := transaction.Prepare()
-				if err != nil {
-					errs <- err
-					return
-				}
-
-				select {
-				case transactions <- transaction:
-					if err != nil {
-						return
-					}
-				case <-cancel:
-					return
-				}
-
-			}
-		}
-	}()
-
-	return transactions
+	return nil
 }
--- engine/transaction.go
+++ engine/transaction.go
@@ -1,20 +1,75 @@
 package engine
 
 import (
+	"sync"
+
 	"github.com/forklift/geppetto/event"
 	"github.com/forklift/geppetto/unit"
 )
 
 func NewTransaction(e *Engine, u *unit.Unit) *Transaction {
-	return &Transaction{engine: e, unit: u}
+	return &Transaction{Explicit: false, engine: e, unit: u, ch: make(chan *event.Event), prepared: false}
+}
+
+func NewTransactionList() TransactionList {
+	return TransactionList{transactions: make(map[string]*Transaction)}
+}
+
+type TransactionList struct {
+	transactions map[string]*Transaction
+	lock         sync.Mutex
+}
+
+func (ts *TransactionList) Add(t *Transaction) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+
+	ts.transactions[t.unit.Name] = t
+}
+
+func (ts *TransactionList) addTo(list *TransactionList) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+
+	for name, t := range ts.transactions {
+		list.transactions[name] = t
+	}
+}
+
+func (ts *TransactionList) Append(list *TransactionList) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+
+	list.addTo(ts)
+}
+
+func (ts *TransactionList) Get(t string) (*Transaction, bool) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+
+	u, ok := ts.transactions[t]
+	return u, ok
+}
+
+func (ts *TransactionList) Drop(t string) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+
+	delete(ts.transactions, t)
 }
 
 type Transaction struct {
+	//An Explicit transaction is directly started by the Engine and is not a dep.
+	// So it should not be "terminated" as dependency.
+	Explicit bool
+
 	engine *Engine
 	unit   *unit.Unit
-	ch     chan<- *event.Event
+	ch     chan *event.Event
 
-	//Internals
+	depslist []string
+
+	deps     TransactionList
 	prepared bool
 }
 
@@ -29,17 +84,18 @@ func (t *Transaction) Prepare() error {
 		return errors.New("Unit is already loaded. Transaction canceled.")
 	}*/
 
-	depslist := []string{}
-	depslist = append(depslist, t.unit.Service.Requires...)
-	depslist = append(depslist, t.unit.Service.Wants...)
+	t.depslist = append(t.unit.Service.Requires, t.unit.Service.Wants...)
 
-	deps, err := t.engine.requestUnits(depslist)
-	if err != nil {
-		return err
+	for _, name := range t.depslist {
+		t, ok := t.engine.Transactions.Get(name)
+		if ok {
+			t.deps.Add(t)
+			continue
+		}
+		//Prepare a new transaction of the dep.
 	}
-	_ = deps
 
-	err = t.unit.Prepare()
+	err := t.unit.Prepare()
 	if err != nil {
 		t.unit.Service.Cleanup()
 		return err
